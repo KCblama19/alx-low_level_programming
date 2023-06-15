778bb2590fdbf5d2331dd5fd275c0e50bdb8ceee
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-15 09:41:28 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
-int main() 
-{
+int main() {
 	puts("Programming is like building a multilingual puzzle");
-	
-	return 0;
+	return (0);
 }
-
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/**Prints with proper grammar, but the outcome is a piece of art,, followed by a new line **/ 
+int main(void) {
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return(0);
+}
