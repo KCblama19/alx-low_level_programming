8224d3791da4c7905f6ab18b3091ef16ae24701e
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-15 09:57:54 -0700

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * prints \"Programming is like building a multilingual puzzle
- * followed by a new line
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void) 
 {
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * Prints with proper grammar, but the outcome is a piece of art,
- * follow by a new line  
+ * follow by a new line.
+ *
+ *
+ * Return: Always 0 (Success)  
  */ 
 int main(void)
 {
