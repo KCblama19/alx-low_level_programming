0dcca5d2d74846997fd1863574aebb6979ac17ad
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-15 14:10:59 -0700

    modify scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,8 +7,8 @@
  * follow by a new line.
  *
  *
- * Return: Always 0 (Success)  
- */ 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
