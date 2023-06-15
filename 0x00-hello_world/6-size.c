cec9fcb3770c33836e8335b10867d522ca3b6534
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-15 14:48:00 -0700

    add 6-size.c script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of int: %d byte(s)\n", sizeof(int));
 	printf("Size of long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float, %d byte(s)\n", sizeof(float));
+	printf("Size of float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
