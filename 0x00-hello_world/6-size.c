12f54c4a76b89b4400898d940c6ac7dda5da382b
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-15 14:36:39 -0700

    add 6-size.c script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float, %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
