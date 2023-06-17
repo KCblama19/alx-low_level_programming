e7a6844d38a1ee71c2028f198d4591e23b4b834e
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-17 06:15:40 -0700

    add 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,15 +10,19 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int numbers;
+	char base16;
 
-	for (numbers = 0; numbers < 10; numbers++)
+	for (base16 = '0'; base16 <= '9'; base16++)
 	{
-		putchar(numbers + '0');
+		putchar(base16);
+	}
+
+	for (base16 = 'a'; base16 <= 'f'; base16++)
+	{
+		putchar(base16);
 	}
 
 	putchar('\n');
 
 	return (0);
 }
-
