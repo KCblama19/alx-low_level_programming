468d6b4503b669b0bd48d219968ff7ea5c589e73
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-17 03:21:51 -0700

    modify 1-las_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 5)
+	int digit = n % 10;
+
+	if (digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	} else if (n == 0)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	} else if (digit < 6 && digit != 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	}
 
 	return (0);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	while ('a' <= 'z')
+	{
+		putchar('a');
+		a++;
+	}
+
+	return (0);
+}
