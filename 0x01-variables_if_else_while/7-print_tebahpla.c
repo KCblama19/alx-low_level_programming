d1d25e65ae3e3a3f7d513cb2f9a8b39b1d9bc374
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-17 05:48:33 -0700

    add 7-print_tebalpha.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char reverseAlphabet;
+
+	for (reverseAlphabet = 'z'; reverseAlphabet > 'a'; reverseAlphabet--)
+	{
+		putchar(reverseAlphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
