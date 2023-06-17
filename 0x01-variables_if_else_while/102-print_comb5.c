3c170e6a63b20588b204807f7dcd6735f0c17149
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-17 09:25:17 -0700

    add 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int digits1, digits2;
+	int count = 0;
+
+	for (digits1 = 0; digits1 <= 99; digits1++)
+	{
+		for (digits2 = digits1 + 1; digits2 <= 99; digits2++)
+		{
+			putchar(digits1 / 10 + '0');
+			putchar(digits1 % 10 + '0');
+			putchar(' ');
+			putchar(digits2 / 10 + '0');
+			putchar(digits2 % 10 + '0');
+
+			if (count != 7)
+			{
+				putchar(',');
+				putchar(' ');
+				count++;
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
