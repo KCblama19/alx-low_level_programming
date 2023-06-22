2c83ad4b729a8a1ce5f282a6dc55ef8a14162403
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-22 04:48:26 -0700

    modify all files

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,13 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (b < a && a > c)
+	else if (a > b && c > a)
 	{
-		largest = a;
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
 	}
 	else
 	{
