4a7b3dcbbf3f85151db42492af255bd3acc0c759
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-22 07:21:57 -0700

    add )-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for upper case character
+ *
+ * @c: recieved integer value
+ *
+ * Return: 1 if c is uppercase
+ *
+ */
+int _isupper(int c);
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contain various c files running dirrent types of functions
