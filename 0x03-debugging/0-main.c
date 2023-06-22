a4000eaa0757f5a34e08cfe93aa71347ca0ab3cd
Abraham K. Blama <abrahamblama19@gmail.com>
2023-06-22 04:02:45 -0700

    debuggin

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#!include "main.h"
+#include "main.h"
 
 /**
  * main - Test function foe positive or negatiive
--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -7,10 +7,11 @@
 
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 98;
-        positive_or_negative(i);
+	i = 98;
 
-        return (0);
+	positive_or_negative(i);
+
+	return (0);
 }
