140a665f093e10771516eae7c984371cbf009d75
Abraham K. Blama <abrahamblama19@gmail.com>
2023-07-05 09:57:02 -0700

    modify files

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,16 +9,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
-	if (*s1 == *s2 || *s2 == '*')
+	if (*s2 == '*')
 	{
-		if (wildcmp(s1 + 1, s2 + 1) == 1)
-			return (1);
+		if (*(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
 
-		if (*s2 == '*' && wildcmp(s1, s2 + 1) == 1)
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
 			return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 
 	return (0);
