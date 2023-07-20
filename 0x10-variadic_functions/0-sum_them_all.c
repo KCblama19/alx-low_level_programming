2f4a28578cffa85314cca3f62b2354ba622df42d
Abraham K. Blama <abrahamblama19@gmail.com>
2023-07-20 09:48:33 -0700

    modify all files

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,18 +12,17 @@
 int sum_them_all(const unsigned int n, ...);
 
 int sum_them_all(const unsigned int n, ...)
-{
+{	
 	va_list list;
+	unsigned int i, sum = 0;
 
 	if (n == 0)
 	{
 		printf("No argument passed ");
 		return (0);
 	}
-	
-	va_start(list, n);
 
-	unsigned int i, sum = 0;
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
