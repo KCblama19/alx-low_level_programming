73a7bb9842a3b2badc030a8c407c5775b78ed1f9
Abraham K. Blama <abrahamblama19@gmail.com>
2023-08-02 07:21:22 -0700

    Detail: add 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees the entire linked list
+ *
+ * @h: pointer to the head of the linked list
+ *
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t length = 0;
+	int diff;
+	listint_t *tempo;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			tempo = (*h)->next;
+			free(*h);
+			*h = tempo;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (length);
+}
